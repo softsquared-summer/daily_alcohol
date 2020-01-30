6abac36d28bcf17f97347644d69106e22432d68e
softsquared-summer <whgusdn528@gmail.com>
2020-01-30 17:06:20 +0900

    softsquared-android-template 적용

---
+++ app/src/main/java/com/softsquared/dailyalcohol/src/main/MainActivity.java
@@ -0,0 +1,46 @@
+package com.softsquared.dailyalcohol.src.main;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.softsquared.dailyalcohol.R;
+import com.softsquared.dailyalcohol.src.BaseActivity;
+import com.softsquared.dailyalcohol.src.main.interfaces.MainActivityView;
+
+
+public class MainActivity extends BaseActivity implements MainActivityView {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private void tryGetTest() {
+        showProgressDialog();
+
+        final MainService mainService = new MainService(this);
+        mainService.getTest();
+    }
+
+    @Override
+    public void validateSuccess(String text) {
+        hideProgressDialog();
+    }
+
+    @Override
+    public void validateFailure(@Nullable String message) {
+        hideProgressDialog();
+        showCustomToast(message == null || message.isEmpty() ? getString(R.string.network_error) : message);
+    }
+
+    public void customOnClick(View view) {
+        switch (view.getId()) {
+        }
+    }
+}
